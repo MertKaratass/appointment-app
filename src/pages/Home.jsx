ab8bd720a29424a6c8c7b37bfd67ccbb2c1f06ec
“red” <karatasmert255@gmail.com>
2023-12-11 23:03:40 +0300

    Update Home component with local storage functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,35 +4,41 @@ import Doctors from "../components/Doctors";
 import { appointmentData } from "../helper/data";
 
 const Home = () => {
-  const [appointments, setAppointments] = useState(appointmentData);
+  // const [appointments, setAppointments] = useState(appointmentData);
+  const [appointments, setAppointments] = useState(
+    JSON.parse(localStorage.getItem("list")) || []
+  );
   const handleAdd = (newAppointment) => {
     setAppointments([...appointments, newAppointment]);
     localStorage.setItem(
-      "list".JSON.stringify([...appointments, newAppointment])
+      "list",
+      JSON.stringify([...appointments, newAppointment])
     );
   };
 
   const handleDelete = (id) => {
     const filteredList = appointments.filter((item) => item.id !== id);
     setAppointments(filteredList);
-    localStorage.setItem("list".JSON.stringify(filteredList));
+    localStorage.setItem("list", JSON.stringify(filteredList));
   };
 
   const handleDoubleClick = (id) => {
-    const updateList = appointments.map((item) =>
+    const updatedList = appointments.map((item) =>
       item.id === id ? { ...item, consulted: !item.consulted } : item
     );
-    setAppointments(updateList);
-    localStorage.setItem("list".JSON.stringify(updateList));
+    setAppointments(updatedList);
+    localStorage.setItem("list", JSON.stringify(updatedList));
   };
+
   return (
     <main className="text-center mt-2">
-      <h1 className="display-5 text-danger">DOM HOSPITAL</h1>
+      <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
       <Doctors handleAdd={handleAdd} />
       <AppointmentList
         appointments={appointments}
         handleDelete={handleDelete}
         handleDoubleClick={handleDoubleClick}
+        // setAppointments={setAppointments}
       />
     </main>
   );
